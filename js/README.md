09eb55bb398f2a7da5ddd4591d1fe36f9c95c6c1
oz-mollerstuen <mollerstuen.works@gmail.com>
2022-10-25 14:13:04 -0700

    Add UI logic
    Co-authored-by: Yoonis Ali <yoonismahamoudali@gmail.com>

--- js/README.md
+++ js/README.md
@@ -75,3 +75,10 @@ Code:
 const text = "Hey you zoinks, muppeteer, biffaroni, loopdaloop person!"
 offensiveWords(text);
 Expected Output: "Hey you person!"
+
+Test: "If an empty string is passed in as a word, it should return 0."
+Code:
+const word = "";
+const text = "red RED Red!";
+numberOfOccurrencesInText(word, text);
+Expected Output: 0
--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,11 @@ function wordCounter(text) {
 }
 
 function numberOfOccurencesInText(word, text) {
+  if (word.trim().length === 0) {
+    return 0;
+  }
+
+  
   const textArray = text.split(' ');
   let wordCount = 0;
   textArray.forEach(function (element) {
@@ -25,20 +30,70 @@ function numberOfOccurencesInText(word, text) {
   return wordCount;
 }
 
-function offensiveWords(word, text) {
-    let badWords = text.split(' ');
-    let badArray = ["zoinks", "muppeteer", "biffaroni", "loopdaloop"];
-    badWords.forEach(function(element, i) {
-      if (badArray.includes(element)) {
-        console.log(badWords.splice(i, 10));
-      } else {
-        console.log(text);
-      }
+// function offensiveWords(word, text) {
+//     let badWords = text.split(' ');
+//     let badArray = ["zoinks", "muppeteer", "biffaroni", "loopdaloop"];
+//     badWords.forEach(function(element, i) {
+//       if (badArray.includes(element)) {
+//         console.log(badWords.splice(i, 10));
+//       } else {
+//         console.log(text);
+//       }
         
-    });
-    console.log(badWords)
-    return badWords.join(" ") 
-  }
+//     });
+//     console.log(badWords)
+//     return badWords.join(" ") 
+//   }
 
-  console.log(offensiveWords("Hey biffaroni You're such a zoinks"))
+//   console.log(offensiveWords("Hey biffaroni You're such a zoinks"))
+
+//-------UI LOGIC--------------------------------------------------------
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const passage = document.getElementById("text-passage").value;
+  const word = document.getElementById("word").value;
+  const wordCount = wordCounter(passage);
+  const occurrencesOfWord = numberOfOccurencesInText(word, passage);
+  document.getElementById("total-count").innerText = wordCount;
+  document.getElementById("selected-count").innerText = occurrencesOfWord;
+  let boldedPassage = boldPassage(word, passage);
+  if (boldedPassage) {
+    document.querySelector("div#bolded-passage").append(boldedPassage);
+  } else {
+    document.querySelector("div#bolded-passage").innerText = null;
+  }
+}
+
+window.addEventListener("load", function() {
+  document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
+
+
+
+});
+
+
+function boldPassage(word, text) {
+  if ((text.trim().length === 0) || (word.trim().length === 0)) {
+    return null;
+  }
+  const p = document.createElement("p");
+  let textArray = text.split(" ");
+  textArray.forEach(function(element, index) {
+    if (word === element) {
+      const bold = document.createElement("strong");
+      bold.append(element);
+      p.append(bold);
+    } else {
+      p.append(element);
+    }
+    if (index !== (textArray.length - 1)) {
+      p.append(" ");
+    }
+  });
+  return p;
+}
+
+
+
 
